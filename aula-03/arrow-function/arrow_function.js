b71131e3645345baef8aa9fdf7d652b22a576e2d
Matheus Carneiro <76215464+mcarneir0@users.noreply.github.com>
2022-03-11 19:42:56 -0300

    Adicionado Aula 03 - Arrow Function

---
+++ aula-03/arrow-function/arrow_function.js
@@ -0,0 +1,15 @@
+//  Estrutura
+//  (argumentos) => { corpo da função }
+
+const soma = (n1, n2) => { return n1 + n2; }
+console.log(soma(1, 2));
+
+
+//  Função com uma única instrução
+const multiplicacao = (n1, n2) => n1 * n2;
+console.log(multiplicacao(2, 3));
+
+
+//  Função com retorno implícito
+const x1 = () => "retorno implícito";
+console.log(x1());
